4c74685ee71ff5372de4fe5d5cc08276d1cff81a
Everton <143721223+EvertonFarias@users.noreply.github.com>
2025-04-23 17:07:42 -0300

    Add files via upload

---
+++ demo/src/test/java/com/AssessPro/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AssessPro.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
